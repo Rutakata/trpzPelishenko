8b1b683d91bf26b790b056843a6e3c2e30bd4af0
Rutakata <vladpelishenko14@gmail.com>
2022-12-05 12:28:41 +0200

    Lab 6

---
+++ lab6/main.py
@@ -0,0 +1,28 @@
+import os
+import re
+
+def sloc(path):
+    total_lines = 0
+    blank_lines = 0
+    comment_lines = 0
+
+    for root, file in os.walk(path):
+        for file in file:
+            if file.endswith(".py"):
+                with open(os.path.join(root, file), 'r') as f:
+                    lines = f.readlines()
+                    total_lines += len(lines)
+
+                    for line in lines:
+                        if line == "\n":
+                            blank_lines += 1
+                            continue
+                        if re.findall(r'(#[^!].*)|(?:\"\"\"(.|\n*)*?\"\"\")|(?:\'\'\'(.|\n*)*?\'\'\')', line):
+                            comment_lines += 1
+
+    comment_stats = comment_lines / total_lines
+    return total_lines, blank_lines, comment_lines, comment_stats
+
+
+sloc = sloc("venv/Lib/site-packages/numpy")
+print(f'Physical SLOC: total lines {sloc[0]}, blank lines {sloc[1]}, comment lines {sloc[2]}, comment ratio {sloc[3]}')
